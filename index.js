276e43613c422e4c9537ec651d328ec3f45f1ce8
nehac710 <167219116+nehac710@users.noreply.github.com>
2024-06-22 22:39:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,27 +21,27 @@ function makesound(key){
         
     case "w": 
 
-        {var tom1 = new Audio("sounds/tom-1.mp3");
+        {var tom1 = new Audio("tom-1.mp3");
         tom1.play();}
          break;
 
     case "a":
-        {var tom2 = new Audio("sounds/tom-2.mp3");
+        {var tom2 = new Audio("tom-2.mp3");
         tom2.play();}
         break;
         
     case "s":
-        {var tom3 = new Audio("sounds/tom-3.mp3");
+        {var tom3 = new Audio("tom-3.mp3");
         tom3.play();}
         break;
     
     case "d":
-        {var tom4 = new Audio("sounds/tom-4.mp3");
+        {var tom4 = new Audio("tom-4.mp3");
         tom4.play();}
         break;
 
     case "j":
-        { var kick = new Audio("sounds/kick-bass.mp3");
+        { var kick = new Audio("kick-bass.mp3");
         kick.play();}
         break;
 
@@ -51,7 +51,7 @@ function makesound(key){
         break;
 
     case "l":
-       {var crash = new Audio("sounds/crash.mp3");
+       {var crash = new Audio("crash.mp3");
         crash.play();}
         break;    
 
